62b975945087c38d232a85181013efd5df22e494
Marcus Wiberg <41900616+bymw@users.noreply.github.com>
2024-12-19 18:56:33 +0100

    latest

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   content: [
     './*.html', // Include HTML files in the root directory
     './**/*.{html,js,jsx,ts,tsx}', // Include HTML, JS, JSX, TS, and TSX files in all subdirectories
-    // Add other file types if necessary, e.g., './**/*.vue' for Vue.js projects
+    '!./node_modules/**/*', // Exclude node_modules from being scanned
   ],
   theme: {
     extend: {},
