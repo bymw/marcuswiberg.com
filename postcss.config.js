f3707007ec17edc9d9a5495e0c3ed66f6dbcbfd0
Marcus Wiberg <41900616+bymw@users.noreply.github.com>
2024-12-19 12:40:57 +0100

    boom code

--- postcss.config.js
+++ postcss.config.js
@@ -3,5 +3,7 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    // Optionally add cssnano for additional minification
+    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}),
   },
 }
