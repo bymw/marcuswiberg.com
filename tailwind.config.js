995d792060880cf86d6909b936200beb4a676185
Marcus Wiberg <41900616+bymw@users.noreply.github.com>
2024-12-22 15:28:49 +0100

    watch installed

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     './**/*.{html,js,jsx,ts,tsx}', // Include HTML, JS, JSX, TS, and TSX files in all subdirectories
     '!./node_modules/**/*', // Exclude node_modules from being scanned
   ],
+  darkMode: 'class', // Enables class-based dark mode
   theme: {
     extend: {},
   },
